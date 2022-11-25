60303728b36501dfbf9809ceb37ff3a5cb4a3403
kjy <kjy@konai.com>
2022-11-25 17:28:56 +0900

    test까지 작성했으나 findbyName, findall 은 스스로 작성해보자

--- hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -1,5 +1,33 @@
 package hello.hellospring.repository;
 
-public class MemoryMemberRepositoryTest {
-    MemberRepository
+import hello.hellospring.domain.Member;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+class MemoryMemberRepositoryTest {
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
+    
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("kjy");
+        repository.save(member);
+        Member result = repository.findById(member.getId()).get();
+    }
+
+    @Test
+    public void findbyName(){
+
+    }
+    @Test
+    public void findAll(){
+
+    }
+
+
 }

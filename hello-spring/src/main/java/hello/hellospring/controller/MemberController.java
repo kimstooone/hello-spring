5feb4ded30e159a16c7c582f4116aa5e38101ca7
kjy <stone9193@gmail.com>
2022-12-15 17:29:33 +0900

    웹 MVC 개발 - 조회
    웹 MVC 개발 - 조회

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -13,13 +16,13 @@ public class MemberController {
     private final MemberService memberService;
 
     @Autowired
-    public MemberController(MemberService memberService){
+    public MemberController(MemberService memberService) {
         //Dependency Injection
         this.memberService = memberService;
     }
 
     @GetMapping("/members/new")
-    public String createForm(){
+    public String createForm() {
         return "members/createMemberForm";
     }
 
@@ -31,4 +34,13 @@ public class MemberController {
 
         return "redirect:/";
     }
+
+    @GetMapping("/members")
+    public String lest(Model model){
+        List<Member> members = memberService.findMembers(); //memberList.html 에서 가져다 쓰게된다...  <tr th:each="member : ${members}">
+        model.addAttribute("members", members);
+        return "members/memberList";
+
+    }
+
 }

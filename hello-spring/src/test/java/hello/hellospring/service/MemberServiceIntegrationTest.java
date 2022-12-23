f8808191cfd495602b666c1b29866a56a73030e2
kjy <stone9193@gmail.com>
2022-12-23 17:26:19 +0900

    JPA의 시작...
    JPA의 시작...

--- hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -42,6 +42,6 @@ public class MemberServiceIntegrationTest {
         memberService.join(member1);
         IllegalStateException e = assertThrows(IllegalStateException.class,
                 () -> memberService.join(member2));//예외가 발생해야 한다.
-        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");
     }
 }

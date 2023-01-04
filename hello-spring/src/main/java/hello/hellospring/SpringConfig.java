1d359694bf9385b796f3edbe50313fc6db342d9e
kjy <stone9193@gmail.com>
2023-01-04 17:36:41 +0900

    JPA
    JPA

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,21 +1,29 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
+import javax.persistence.EntityManager;
 
 //자바 코드로 직접 스프링 빈 등록하기
 //상황에 따라 구현 클래스를 변경해야 하면 설정을 통해 스프링 빈으로 등록한다.
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
+    /*private final DataSource dataSource;
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
+    }*/
+
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -24,8 +32,9 @@ public class SpringConfig {
     }
     @Bean
     public MemberRepository memberRepository() {
-// return new MemoryMemberRepository();
-// return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+//      return new MemoryMemberRepository();
+//      return new JdbcMemberRepository(dataSource);
+//      return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
---
+++ hello-spring/src/main/java/hello/hellospring/repository/JpaMemberRepository.java
@@ -0,0 +1,46 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+public class JpaMemberRepository implements MemberRepository {
+
+    //JPA 쓸려면 필수 내부적으로 DB 정보를 다 가지고있음
+    private final EntityManager em;
+
+    public JpaMemberRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    @Override
+    public Member save(Member member) {
+        em.persist(member); // 이것만으로도 쿼리문 없이 insert 가능
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        List<Member> result = em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        //객체를 대상으로 쿼리를 날리면 SQL로 번역된다.
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+}
